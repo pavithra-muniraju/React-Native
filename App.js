0addd0af3384724334f96e74d0d9d2cc4ed1a67d
Pavithra Muniraju <pavithra.muniraju@ust.com>
2025-09-25 17:18:17 +0530

    mini-game

--- App.js
+++ App.js
@@ -42,7 +42,6 @@ export default function App() {
     screen = <GameScreen userNumber={userNumber} onGameOver={gameOverHandler}/>;
   }
 
-
   if (gameOver && userNumber) {
     screen = <GameOverScreen
      roundNumber={guessRounds} 
