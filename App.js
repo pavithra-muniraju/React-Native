19456258c5bb4df21d36268147d5a08c31c42241
Pavithra Muniraju <pavithra.muniraju@ust.com>
2025-10-10 12:07:48 +0530

    meals app with react navigation package

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import CategoriesScreen from './screens/CategoriesScreen';
 
+const Stack = createNativeStackNavigator();
+
 export default function App() {
-   return (
+  return (
     <>
-      <StatusBar style='light' />
-      <CategoriesScreen />
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: {},
 });
