e1baee81dd3e3c72da18334375cbe7586eae392f
Pavithra Muniraju <pavithra.muniraju@ust.com>
2025-10-15 13:52:49 +0530

    home page added

--- App.js
+++ App.js
@@ -1,39 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import CategoriesScreen from './screens/CategoriesScreen';
-import MealsOverviewScreen from './screens/MealsOverviewScreen';
-import MealDataScreen from './screens/MealDataScreen';
-// https://github.com/academind/react-native-practical-guide-code ---> full guide
-const Stack = createNativeStackNavigator();
+import HomePage from './screens/HomePage';
 
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <>
-      <StatusBar style="light" />
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{
-            
-            headerStyle: {backgroundColor: '#351401'},
-            headerTintColor: 'white',
-            contentStyle: {backgroundColor: '#bc6834ff'}
-          }}>
-          <Stack.Screen name="MealsCategories" component={CategoriesScreen} options={{title: 'All Categories',}}  />
-          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} 
-          options={({route, navigation}) => {
-            const catId = route.params.categoryId;
-            return {
-              title: catId
-            }
-          }} />
-          <Stack.Screen name='MealData' component={MealDataScreen}></Stack.Screen>
-        </Stack.Navigator>
-      </NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name='Home' component={HomePage} />
+      </Stack.Navigator>
+    </NavigationContainer>
     </>
-  );
+  )
 }
-
 const styles = StyleSheet.create({
   container: {},
 });
